2c8c216f30763b1a81c79f3ff956540faa01598d
SPrise810 <104680134+SPrise810@users.noreply.github.com>
2023-09-11 21:34:16 +0800

    Add files via upload

--- 5_4_27_new/5_4_27_new/test.c
+++ 5_4_27_new/5_4_27_new/test.c
@@ -1,1 +1,35 @@
 //1.先上节课收尾
+
+////1. 删除链表中等于给定值 val 的所有节点。 OJ链接
+//https://leetcode.cn/problems/remove-linked-list-elements/description/
+
+struct ListNode* removeElements(struct ListNode* head,int val)
+{
+	struct ListNode* prev = NULL;
+	struct ListNode* cur = head;
+	while(cur)
+	{
+
+		if (cur->val == val)//有需要删除的情况
+		{
+			//头删
+			if (cur == head)
+			{
+				head = cur->next;
+				free(cur);
+				cur = head;
+			}
+			else//非头删
+			{
+				prev->next = cur->next;
+				free(cur);
+				cur = prev->next;
+			}
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+	};
+};
